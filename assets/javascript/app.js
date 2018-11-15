6fcc5fe00cd55aa0b98211fcd601701b0ce81095
KitKat <katherine_nguyen7@yahoo.com>
2018-11-15 02:39:16 -0800

    updated nav links

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,23 +11,17 @@ $(document).ready(function() {
 
     // Displays About section on click //
     $("#about").on("click", function() {
-        $(".about-section").show();
-        $(".portfolio-section").hide();
-        $(".contact-section").hide();
+      
     })
 
     // Displays Portfolio section on click //
     $("#portfolio").on("click", function() {
-        $(".portfolio-section").show();
-        $(".about-section").hide();
-        $(".contact-section").hide();
+       
     })
 
     // Displays Contact section on click //
     $("#contact").on("click", function() {
-        $(".contact-section").show();
-        $(".about-section").hide();
-        $(".portfolio-section").hide();
+      
     })
 
 });
