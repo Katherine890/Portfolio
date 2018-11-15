e8ba997fb6f87435362d317dddbdf1a8644b123a
KitKat <katherine_nguyen7@yahoo.com>
2018-11-14 16:22:44 -0800

    updated About Me

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,18 +9,21 @@ function closeNav() {
 // Fade in Heading Links //
 $(document).ready(function() {
 
+    // Displays About section on click //
     $("#about").on("click", function() {
         $(".about-section").show();
         $(".portfolio-section").hide();
         $(".contact-section").hide();
     })
 
+    // Displays Portfolio section on click //
     $("#portfolio").on("click", function() {
         $(".portfolio-section").show();
         $(".about-section").hide();
         $(".contact-section").hide();
     })
 
+    // Displays Contact section on click //
     $("#contact").on("click", function() {
         $(".contact-section").show();
         $(".about-section").hide();
